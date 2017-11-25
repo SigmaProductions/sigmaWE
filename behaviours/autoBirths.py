e85320be906399aaf2386f62f1aed22119e6013c
kipic96 <kipic96@gmail.com>
2017-11-25 14:19:44 +0100

    Not working marian side

--- behaviours/autoBirths.py
+++ behaviours/autoBirths.py
@@ -4,30 +4,27 @@ from fbchat.models import *
 from datetime import datetime
 import pickle
 import time
+import threading
 
 
-class AutoBirths(IBehaviourBase):
-    def __init__(self):
+class behaviourClass(IBehaviourBase):
+    def __init__(self, email, password, kwargs):
+        IBehaviourBase.__init__(email, password, kwargs)
         self.userBirthdays = dict()
-        self.client = fbchat.Client("dddf@koszmail.pl", "przemekssie")
-
-
+        self.listening = ListeningBirthThreads(self)
+        self.listening.Run()
 
     def Run(self):
-        self.userBirthdays = self.readPickle()
-       # self.userBirthdays = dict()
-       # self.userBirthdays.update({str(100004346340674): datetime.strptime('25 Nov 2017', '%d %b %Y')})
-        now = datetime.now()
-        now = now.replace(now.year, now.month, now.day, 0, 0, 0, 0)
-        self.checkAllBirths(now)
-        self.writePickle(self.userBirthdays)
-        time.sleep(1000)
         while True:
-            print('fuck')
+            self.userBirthdays = self.readPickle()
+            now = datetime.now()
+            now = now.replace(now.year, now.month, now.day, 0, 0, 0, 0)
+            self.checkAllBirths(now)
+            self.writePickle(self.userBirthdays)
 
     def writePickle(self, userBirthds):
         with open('birthdays.pickle', 'wb') as handle:
-            pickle.dump(self.userBirthdays, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            pickle.dump(userBirthds, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     def readPickle(self):
         with open('birthdays.pickle', 'rb') as handle:
@@ -43,7 +40,28 @@ class AutoBirths(IBehaviourBase):
         try:
             return dat.replace(year = dat.year + 1)
         except ValueError:
-            return dat + (datetime.date(dat.year + 1, 1, 1) - datetime.date(dat.year, 1, 1))
+            return dat + (datetime(dat.year + 1, 1, 1) - datetime(dat.year, 1, 1))
 
-bir = AutoBirths()
-bir.Run()
+    def onMessage(self, author_id, message, thread_id, thread_type, **kwargs):
+        births = self.readPickle()
+        lines = message.split()
+        if lines.__len__() == 4:
+            userID = lines[0]
+            day = lines[1]
+            month = lines[2]
+            year = lines[3]
+            newDate = datetime(year)
+            newDate = newDate.replace(year, month, day, 0, 0, 0, 0)
+            births.update({userID: datetime.date(newDate)})
+        self.writePickle(births)
+
+
+class ListeningBirthThreads:
+    def __init__(self, birthClient):
+       # self.client = client
+        self.listenThread = threading.Thread(target=birthClient.listen)
+
+    def Run(self):
+        self.listenThread.start()
+
+
--- tests/GetAllActionsTest.py
+++ tests/GetAllActionsTest.py
@@ -6,20 +6,18 @@ from model import account
 firstAccount = account.Account("dddf@koszmail.pl", "przemekssie")
 secondAccount = account.Account("email1", "haslo1")
 
-
-
 am = actionsManager.Actions()
-
 
 modulesMan= modulesManager.moduleManager()
 modulesMan.loadModules(["hydra"])
 hydrModule= modulesMan.getModule("hydra")
 
+modulesMan.loadModules(["autoBirths"])
+birthModule=modulesMan.getModule("autoBirths")
 
 
-
-
-am.AddAction(firstAccount,hydrModule,{"CONFID":"1668412989883731"})
+am.AddAction(firstAccount, hydrModule,{"CONFID":"1668412989883731"})
+am.AddAction(firstAccount, birthModule, {})
 
 test = am.GetAllActionsRunnedOnAccount("dddf@koszmail.pl")
 input("s")
