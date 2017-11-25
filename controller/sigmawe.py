9f490e9720581db81d3c0ef16311d84c8ce70a20
MacenDi <mstanekofficial@gmail.com>
2017-11-25 01:17:04 +0100

    Controller Refactor

--- controller/sigmawe.py
+++ controller/sigmawe.py
@@ -1,14 +1,30 @@
 from helper.readUsersFromFile import readUsersFromFile
 from model import accountManager as am
+from model import actionsManager as actionM
 
 
 class SigmaWE:
 
     def __init__(self):
-        self.accManager = am.AccountManager()
+        self.AccManager = am.AccountManager()
         self.ModuleManager=None
+        self.ActionManager = actionM.Actions()
 
-    def addAcountsFromFile(self):
+
+    def AddAcountsFromFile(self):
         users = readUsersFromFile()
-        self.accManager.AddMultipleAccounts(users)
+        self.AccManager.AddMultipleAccounts(users)
 
+    def AddSingleAccount(self, email, password):
+        self.AccManager.AddAccount(email,password)
+
+    def RemoveSingleAccount(self, email):
+        self.AccManager.RemoveAccount(email)
+
+    def LoadBehaviourOnAccount(self, email, moduleName, kwargs):
+        self.ActionManager.AddAction(self.AccManager.GetAccount(email), moduleName, kwargs)
+
+
+
+    def LoadModules(self,moduleName):
+        self.ModuleManager.loadModules([moduleName])
